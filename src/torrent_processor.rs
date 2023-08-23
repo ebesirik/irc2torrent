11f084f5759db1586135abf0deea0b5767a07443
Emre Besirik <nomercy@gmail.com>
2023-08-23 23:04:35 +0300

    Config to its own object and logic for Commands feature #1, progress 2

--- src/torrent_processor.rs
+++ src/torrent_processor.rs
@@ -9,21 +9,21 @@ use tokio::fs;
 use lava_torrent::torrent::v1::Torrent;
 use regex::Regex;
 use crate::config::Config;
+use crate::OPTIONS_CONFIG_FILE;
 
 pub struct TorrentProcessor<'tp> {
     rss_key: String,
     rtorrent_url: String,
     torrent_files_dir: PathBuf,
     torrent_match_regex_list: Vec<Regex>,
-    options: &'tp Config
+    options: &'tp Config,
 }
 
-impl <'tp>TorrentProcessor<'tp> {
-
-    pub fn new(rss:String, url: String, dir: PathBuf, config: &'tp Config) -> Self{
+impl<'tp> TorrentProcessor<'tp> {
+    pub fn new(rss: String, url: String, dir: PathBuf, config: &'tp Config) -> Self {
         Self { rss_key: rss, rtorrent_url: url, torrent_files_dir: dir.join("torrent_files/"), torrent_match_regex_list: config.get_dl_regexes(), options: config }
     }
-    
+
     pub fn do_we_want_this_torrent(&self, name: &String) -> bool {
         for regex in &self.torrent_match_regex_list {
             if regex.is_match(name) {
@@ -33,7 +33,7 @@ impl <'tp>TorrentProcessor<'tp> {
         return false;
     }
 
-    pub async fn get_dl_list(&self){
+    pub async fn get_dl_list(&self) {
         let url = Url::parse(&self.rtorrent_url).unwrap();
         let client: Client = ClientBuilder::new(url)
             .user_agent("dxr-client-example")
@@ -43,7 +43,7 @@ impl <'tp>TorrentProcessor<'tp> {
         match result {
             Ok(r) => {
                 info!("Torrent load result: {r:?}");
-            },
+            }
             Err(e) => {
                 error!("Error loading torrent: {:?}", e);
             }
@@ -56,7 +56,7 @@ impl <'tp>TorrentProcessor<'tp> {
             .user_agent("dxr-client-example")
             .build();
         //println!("{}", file);
-        if let Ok(bytes) = base64::decode(file){
+        if let Ok(bytes) = base64::decode(file) {
             let request = Call::new("load.raw_start_verbose", ("", bytes.as_slice()));
             // let request = dxr::client::Call::new("load.raw_start_verbose", file);
             let result = client.call(request).await as Result<i32, anyhow::Error>;
@@ -71,12 +71,12 @@ impl <'tp>TorrentProcessor<'tp> {
                     this line should be added to your .rtorrent.rc file for next line to work;
                     method.insert = fix_addtime, simple, "d.custom.set=addtime,(cat,$d.creation_date=)"
                     */
-                    let fix_time : Call<String, i32> = Call::new("fix_addtime", the_hash);
+                    let fix_time: Call<String, i32> = Call::new("fix_addtime", the_hash);
                     match client.call(fix_time).await {
                         Ok(_) => info!("Time fixed for {}", name),
                         Err(e2) => error!("Error fixing time {:?},\n\t did you forget to add this line: \n\\t\t'{}'\n\t to your .rtorrent.rc file?", e2, "method.insert = fix_addtime, simple, \"d.custom.set=addtime,(cat,$d.creation_date=)\"")
                     }
-                },
+                }
                 Err(e) => println!("File upload err: ({}) {:?}", name, e),
             }
         }
@@ -88,7 +88,7 @@ impl <'tp>TorrentProcessor<'tp> {
 
         let torrent_file = name.replace(" ", ".") + ".torrent";
         if let Ok(resp) = reqwest::get(format!("https://www.torrentleech.org/rss/download/{}/{}/{}", id, &self.rss_key, torrent_file)).await {
-            if let Ok(bytes) = resp.bytes().await{
+            if let Ok(bytes) = resp.bytes().await {
                 if !self.torrent_files_dir.exists() {
                     let _ = fs::create_dir(&self.torrent_files_dir).await;
                 }
@@ -99,5 +99,31 @@ impl <'tp>TorrentProcessor<'tp> {
             }
         }
         Err("Failed to download file".to_string())
+    }
+
+    pub async fn add_torrent(&self, name: &str, id: &str) -> Result<String, String> {
+        if let Ok(b64) = self.download_torrent(name.to_string(), id.to_string()).await {
+            self.add_torrent_and_start(b64, name.to_string()).await;
+            return Ok(format!("Torrent {} added to rtorrent", name));
+        }
+        Err("Can not download torrent file".to_string())
+    }
+
+    pub fn add_torrent_to_watchlist(mut self, argument: String) -> Result<String, String> {
+        let mut err_str = "Regez format error. Use: addtowatchlist: <regex>";
+        if let Ok(rgx) = Regex::new(argument.as_str()) {
+            self.torrent_match_regex_list.push(rgx);
+            self.options.add_dl_regex(argument);
+            return Ok(format!("Torrent {} added to watch list", argument));
+        }
+        Err(err_str.to_string())
+    }
+
+    pub fn remove_torrent_from_watchlist(&mut self, index: usize) -> Result<String, String> {
+        if index < self.torrent_match_regex_list.len() {
+            self.torrent_match_regex_list.remove(index);
+            return Ok(format!("Torrent {} removed from watch list", index));
+        }
+        Err("Index out of range".to_string())
     }
 }
